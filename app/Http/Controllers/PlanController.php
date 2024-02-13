19b4d173adc784c884f7306832ce919d68f09cf9
Muhammad Maaz Faisal <m.maazfaisal0301@gmail.com>
2024-02-13 22:34:21 +0500

    first commit

---
+++ app/Http/Controllers/PlanController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plan;
+use Illuminate\Http\Request;
+use Laravel\Cashier\Billable;
+
+class PlanController extends Controller
+{
+    use Billable;
+    public function index()
+    {
+        $plans = Plan::get();
+
+        return view("plans", compact("plans"));
+    }
+
+    public function show(Plan $plan, Request $request)
+    {
+        // dd($plan, $request);
+        $intent = auth()->user()->createSetupIntent();
+        // dd($intent);
+        return view("subscription", compact("plan", "intent"));
+    }
+
+    public function subscription(Request $request)
+    {
+        $plan = Plan::find($request->plan);
+        // dd($plan);
+        // dd($request->plan, $plan->stripe_plan, $request->token );
+        $subscription = $request->user()->newSubscription($request->plan, $plan->stripe_plan)
+                        ->create($request->token);
+
+        return view("subscription_success");
+    }
+}
---
+++ database/seeders/PlanSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Plan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PlanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $plans = [
+            [
+                'name' => 'Basic',
+                'slug' => 'basic',
+                'stripe_plan' => 'price_1OjOpnHQk1REbrVJ1YDeM3Gk',
+                'price' => 10,
+                'description' => 'Basic'
+            ],
+            [
+                'name' => 'Premium',
+                'slug' => 'premium',
+                'stripe_plan' => 'price_1OjOqRHQk1REbrVJH6BnUr0h',
+                'price' => 100,
+                'description' => 'Premium'
+            ]
+        ];
+
+        foreach ($plans as $plan) {
+            Plan::create($plan);
+        }
+    }
+
+}
